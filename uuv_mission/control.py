b69a20ffe1ca98006f865fc92da8a06616e84d9e
Lachlan Dow <lachlan.dow@stcatz.ox.ac.uk>
2024-10-22 11:09:24 +0100

    Renamed variables in the control module for clarity, experimented with various values of proportional and derivative gain,  cleaned up the code in the Jupyter notebook.

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,12 +1,12 @@
 #Code that implements the PD controller.
 class PDController:
-    def __init__(self, KP=0.15, KD=0.6):
-        self.KP = KP
-        self.KD = KD
+    def __init__(self, proportional_gain=0.15, derivative_gain=0.6):
+        self.proportional_gain = proportional_gain
+        self.derivative_gain = derivative_gain
         self.previous_error = 0
 
     def compute_control_action(self, error):
         derivative = error - self.previous_error
-        control_action = self.KP * error + self.KD * derivative
+        control_action = self.proportional_gain * error + self.derivative_gain * derivative
         self.previous_error = error
         return control_action
